f03d6dc243585bc5f94c9e20c202caaaea208952
unknown <markmark092100@gmail.com>
2025-01-31 12:21:51 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,25 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/roll-uiiai/',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: 'index.html'
-      }
-    }
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|mp3|mp4/i.test(ext)) {
+            return `assets/media/[name][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
   },
   publicDir: 'public'
 })
